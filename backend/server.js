f64c961790a74737c4034a816e9055285f486905
Sanket Kumar <kumarsanket520@gmail.com>
2026-07-18 23:00:28 +0530

    fixing_parse_error

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,9 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Routes
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/user.routes"));
